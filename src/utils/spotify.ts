bb72f5e56408a53d668201b53bfb4e37cdd7fe45
lorre <lorre.huggan@protonmail.com>
2023-02-24 01:19:12 +0000

    chore: update env

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -9,7 +9,7 @@ export const spotifyApi = new SpotifyWebApi({
 const DEV_URL = 'http://localhost:3000/';
 const PROD_URL = 'https://www.sazaana.com/';
 
-export const loginURL = `https://accounts.spotify.com/authorize?client_id=1a98caa930f04397b79800309b24b9c5&response_type=code&redirect_uri=${DEV_URL}&scope=ugc-image-upload%20user-read-recently-played%20user-read-playback-state%20user-top-read%20app-remote-control%20playlist-modify-public%20user-modify-playback-state%20playlist-modify-private%20user-follow-modify%20user-read-currently-playing%20user-follow-read%20user-library-modify%20user-read-playback-position%20playlist-read-private%20user-read-email%20user-read-private%20user-library-read%20playlist-read-collaborative%20streaming&state${'x'}&show_dialog=true`;
+export const loginURL = `https://accounts.spotify.com/authorize?client_id=1a98caa930f04397b79800309b24b9c5&response_type=code&redirect_uri=${PROD_URL}&scope=ugc-image-upload%20user-read-recently-played%20user-read-playback-state%20user-top-read%20app-remote-control%20playlist-modify-public%20user-modify-playback-state%20playlist-modify-private%20user-follow-modify%20user-read-currently-playing%20user-follow-read%20user-library-modify%20user-read-playback-position%20playlist-read-private%20user-read-email%20user-read-private%20user-library-read%20playlist-read-collaborative%20streaming&state${'x'}&show_dialog=true`;
 
 export async function refreshAccessToken(token: any) {
   try {
