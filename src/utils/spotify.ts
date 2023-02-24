3f8c633d4e863ad36adde29fb8e6b767c98e37da
lorre <lorre.huggan@protonmail.com>
2023-02-24 14:46:18 +0000

    chore: move url string to env variable

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -6,8 +6,8 @@ export const spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 });
 
-const DEV_URL = 'http://localhost:3000/';
-const PROD_URL = 'https://www.sazaana.com/';
+const DEV_URL = process.env.DEV_URL;
+const PROD_URL = process.env.PROD_URL;
 
 export const loginURL = `https://accounts.spotify.com/authorize?client_id=1a98caa930f04397b79800309b24b9c5&response_type=code&redirect_uri=${PROD_URL}&scope=ugc-image-upload%20user-read-recently-played%20user-read-playback-state%20user-top-read%20app-remote-control%20playlist-modify-public%20user-modify-playback-state%20playlist-modify-private%20user-follow-modify%20user-read-currently-playing%20user-follow-read%20user-library-modify%20user-read-playback-position%20playlist-read-private%20user-read-email%20user-read-private%20user-library-read%20playlist-read-collaborative%20streaming&state${'x'}&show_dialog=true`;
 
