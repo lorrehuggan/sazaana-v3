43ef322e8f95c45dc33bef64fc73ba549129cfd4
lorrehuggan <lorre.huggan@protonmail.com>
2023-06-18 17:51:26 +0100

    chore: fix eslint issues

--- src/components/playlist/TrackImage.tsx
+++ src/components/playlist/TrackImage.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@components/ui/Box';
+import React from "react";
+import { Box } from "@components/ui/Box";
 
 interface Props {
   trackPlayingId: string;
@@ -9,22 +10,22 @@ const TrackImage = ({ trackPlayingId, track }: Props) => {
   return (
     <Box
       css={{
-        position: 'relative',
-        rotate: trackPlayingId === track.preview_url ? '3deg' : '0deg',
-        transition: 'rotate 0.3s ease-in-out',
+        position: "relative",
+        rotate: trackPlayingId === track.preview_url ? "3deg" : "0deg",
+        transition: "rotate 0.3s ease-in-out",
         zIndex: trackPlayingId === track.preview_url ? 10 : 0,
       }}
     >
       <Box>
         <img
           style={{
-            objectFit: 'cover',
-            borderRadius: '4px',
+            objectFit: "cover",
+            borderRadius: "4px",
           }}
           width={track.album?.images[2]?.width}
           height={track.album?.images[2]?.height}
-          src={track.album?.images[2]?.url!}
-          alt={track.name!}
+          src={track.album?.images[2]?.url}
+          alt={track.name}
         />
       </Box>
     </Box>
