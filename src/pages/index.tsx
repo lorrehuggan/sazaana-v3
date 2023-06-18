f44818371eb92601cc29ec8d8dd4b7c4ff76b3a1
lorrehuggan <lorre.huggan@protonmail.com>
2023-06-18 23:47:26 +0100

    feat: home page redirects to a random artist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,19 @@ import Following from "@components/user/Following";
 import { useSession } from "next-auth/react";
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const artists = [
+    "06HL4z0CvFAxyc27GXpf02",
+    "1RyvyyTE3xzB2ZywiAwp0i",
+    "2wY79sveU1sp5g7SokKOiI",
+    "3TVXtAsR1Inumwj472S9r4",
+    "6eUKZXaKkcviH0Ku9w2n3V",
+    "2WoVwexZuODvclzULjPQtm",
+  ];
+  const randomArtist = artists[Math.floor(Math.random() * artists.length)];
   return {
     redirect: {
       permanent: false,
-      destination: `/playlist/${process.env.FEATURED_ARTIST_ID}`,
+      destination: `/playlist/${randomArtist}`,
     },
   };
 };
